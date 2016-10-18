bc0e7d41d566c66db9b39b4b1e6f0cea8e309b67
jichg1986 <84362484@qq.com>
2016-10-18 09:16:02 +0800

    分了俩个内容

---
+++ 分包管理/FASTPAN/src/main/java/com/fastpan/dao/mapper/test/TestMapper.java
@@ -0,0 +1,15 @@
+package com.fastpan.dao.mapper.test;
+
+import java.util.List;
+
+
+import com.fastpan.dao.pojo.test.Test;
+
+public interface TestMapper {
+	List<Test> queryob();
+    int insertTest (Test test);
+    
+    List<Test> selectAll();
+    
+    int delete(Test test); 
+}
---
+++ 分包管理/FASTPAN/src/main/java/com/fastpan/dao/pojo/test/Test.java
@@ -0,0 +1,34 @@
+package com.fastpan.dao.pojo.test;
+
+public class Test {
+	private int id ;
+	private String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Test() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Test(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Test [id=" + id + ", name=" + name + "]";
+	}
+	
+	
+	
+}
